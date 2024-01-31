bfbb035485465e0f01e4680e661042d3cfe304a3
LockHacks <anathisjadudlamini@gmail.com>
2024-01-31 00:42:03 -0800

    100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -56,7 +56,7 @@ def matrix_mul(m_a, m_b):
     mul_matrix = []
 
     for row_1 in m_a:
-        l = 0
+        l_row = 0
         l_row = []
         while l < len(m_b[0]):
             result = 0
